04d7a75c820a63cb99e075869a0ca7fe2dd40bb4
An Lin <august810511@gmail.com>
2024-04-21 23:39:52 +0800

    feat: add category model

--- models/viewpoint.js
+++ models/viewpoint.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   Viewpoint.associate = function(models) {
     // associations can be defined here
+    Viewpoint.belongsTo(models.Category, { foreignKey: 'categoryId' })
   };
   return Viewpoint;
 };
