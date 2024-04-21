c0932abccb22342b3baaf09f79f2e65df95d6111
An Lin <august810511@gmail.com>
2024-04-21 23:31:30 +0800

    feat: add viewpoint model

---
+++ models/viewpoint.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Viewpoint = sequelize.define('Viewpoint', {
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    image: DataTypes.STRING,
+    category_id: DataTypes.INTEGER
+  }, {
+    underscored: true,
+  });
+  Viewpoint.associate = function(models) {
+    // associations can be defined here
+  };
+  return Viewpoint;
+};
