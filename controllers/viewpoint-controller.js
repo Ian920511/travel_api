9803ddc3a2ccfc843ac27ce7b1c7c60820d22196
An Lin <august810511@gmail.com>
2024-04-22 01:10:40 +0800

    feat: create viewpoint

--- controllers/viewpoint-controller.js
+++ controllers/viewpoint-controller.js
@@ -1,4 +1,6 @@
 const { Viewpoint } = require('../models')
+const { Category } = require('../models')
+const createError = require('http-errors')
 
 const viewpointController = {
   getViewpoints: async (req, res, next) => {
@@ -9,6 +11,34 @@ const viewpointController = {
         status: 'success',
         data: {
           viewpoints
+        }
+      })
+
+
+    } catch (error) {
+      next (error)
+    }
+  },
+
+  postViewpoints: async (req, res, next) => {
+    try {
+      const { name, address, image, categoryId } = req.body
+
+      if (!name) throw new createError(400, 'Viewpoint name is required')
+
+      const category = await Category.findByPk(categoryId, { raw: true })
+
+      const viewpoint = await Viewpoint.create({
+        name,
+        address,
+        image,
+        categoryId: category.id
+      })
+
+      res.json({
+        status: 'success',
+        data: {
+          viewpoint
         }
       })
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,5 +8,6 @@ const viewpointController = require('./../../controllers/viewpoint-controller')
 router.post('/category', categoryController.postCategories)
 router.put('/category/:id', categoryController.putCategory)
 router.delete('/category/:id', categoryController.deleteCategory)
+router.post('/viewpoint', viewpointController.postViewpoints)
 
 module.exports = router
