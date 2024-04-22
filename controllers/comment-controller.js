2da9a5dc7ef4cc55dcf39ed09e8d4137384c36a8
An Lin <august810511@gmail.com>
2024-04-22 11:22:28 +0800

    feat: post comment

---
+++ controllers/comment-controller.js
@@ -0,0 +1,38 @@
+const { Comment, User, Viewpoint } = require('../models')
+const createError = require('http-errors')
+
+const commentController = {
+  postComment: async (req, res, next) => {
+    try {
+      const { viewpointId, text } = req.body
+      const userId = req.user.id
+
+      if (!text) throw new createError(400, 'Comment text is required')
+
+      const user = await User.findByPk(userId)
+      if (!user) throw new createError(404, "User didn't exist")
+
+      const viewpoint = await Viewpoint.findByPk(viewpointId)
+      if (!viewpoint) throw new createError(404, "Viewpoint didn't exist")
+
+      const comment = await Comment.create({
+        text,
+        viewpointId,
+        userId
+      })
+
+      res.json({
+        status: 'success',
+        data: { comment }
+      })
+
+    } catch (error) {
+      next (error)
+    }
+  },
+
+
+
+}
+
+module.exports = commentController
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const users = require('./modules/users')
 const categories = require('./modules/categories')
 const admin = require('./modules/admin')
 const viewpoints = require('./modules/viewpoints')
+const comments = require('./modules/comments')
 
 router.use('/users', users)
 router.use('/categories', categories)
 router.use('/viewpoints', viewpoints)
 router.use('/admin', authenticated, admin)
+router.use('/comments', authenticated, comments)
 
 module.exports = router
